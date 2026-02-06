e3d66064eaa9431f8431e5e10745c9edf85064e8
fluffylenny <fluffylennys@gmail.com>
2026-02-05 23:38:55 -0800

    added checks for low value pages and excessively large webpages before scraping

--- scraper.py
+++ scraper.py
@@ -5,13 +5,188 @@ from urllib.parse import urlparse, parse_qs, urldefrag, urljoin
 # RESOURCE:
 # Beautiful Soup: https://medium.com/@spaw.co/beautifulsoup-find-all-421385b341d4 
 
+STOPWORDS = {"a",
+             "about",
+             "above",
+             "after",
+             "again",
+             "against",
+             "all",
+             "am",
+             "an",
+             "and",
+             "any",
+             "are",
+             "aren't",
+             "as",
+             "at",
+             "be",
+             "because",
+             "been",
+             "before",
+             "being",
+             "below",
+             "between",
+             "both",
+             "but",
+             "by",
+             "can't",
+             "cannot",
+             "could",
+             "couldn't",
+             "did",
+             "didn't",
+             "do",
+             "does",
+             "doesn't",
+             "doing",
+             "don't",
+             "down",
+             "during",
+             "each",
+             "few",
+             "for",
+             "from",
+             "further",
+             "had",
+             "hadn't",
+             "has",
+             "hasn't",
+             "have",
+             "haven't",
+             "having",
+             "he",
+             "he'd",
+             "he'll",
+             "he's"
+             "her",
+             "here",
+             "here's",
+             "hers",
+             "herself"
+             "him,"
+             "himself",
+             "his",
+             "how",
+             "how's",
+             "i",
+             "i'd",
+             "i'll",
+             "i'm",
+             "i've",
+             "if,"
+             "in",
+             "into",
+             "is",
+             "isn't",
+             "it",
+             "it's",
+             "its",
+             "itself",
+             "let's",
+             "me",
+             "more",
+             "most",
+             "mustn't",
+             "my",
+             "myself",
+             "no",
+             "nor",
+             "not",
+             "of",
+             "off",
+             "on",
+             "once",
+             "only",
+             "or",
+             "other",
+             "ought",
+             "our",
+             "ours",
+             "ourselves",
+             "out",
+             "over",
+             "own",
+             "same",
+             "shan't",
+             "she",
+             "she'd",
+             "she'll",
+             "she's",
+             "should",
+             "shouldn't",
+             "so",
+             "some",
+             "such",
+             "than",
+             "that",
+             "that's",
+             "the",
+             "their",
+             "theirs",
+             "them",
+             "themselves",
+             "then",
+             "there",
+             "there's",
+             "these",
+             "they",
+             "they'd",
+             "they'll",
+             "they're",
+             "they've",
+             "this",
+             "those",
+             "through",
+             "to",
+             "too",
+             "under",
+             "until",
+             "up",
+             "very",
+             "was",
+             "wasn't",
+             "we",
+             "we'd",
+             "we'll",
+             "we're",
+             "we've",
+             "were",
+             "weren't",
+             "what",
+             "what's",
+             "when",
+             "when's",
+             "where",
+             "where's",
+             "which",
+             "while",
+             "who",
+             "who's",
+             "whom",
+             "why",
+             "why's",
+             "with",
+             "won't",
+             "would",
+             "wouldn't",
+             "you",
+             "you'd",
+             "you'll",
+             "you're",
+             "you've",
+             "your",
+             "yours",
+             "yourself",
+             "yourselves"}
+
 def scraper(url, resp):
     """
     Given a URL and a Response object from the cache server,
     return a list of valid outgoing links found on the page.
     """
 
-    if not status_check(resp):
+    if not status_check(resp) or low_value(resp) or too_large(resp):
         return []
 
     #extract links using the provided helper, then filter with is_valid
@@ -49,6 +224,24 @@ def extract_next_links(url, resp) -> list:
         linkstrings.add(final_url)
 
     return list(linkstrings)
+
+# Content-Length header may be inaccurate but it's more efficient than counting words
+def low_value(resp):
+    words = resp.text.split()
+    count = 0
+
+    for word in words:
+        if word.lower() not in STOPWORDS:
+            count += 1
+
+    return count < 200 #can change number later
+
+def too_large(resp):
+    content_length = resp.headers.get('Content-Length')
+    if content_length:
+        return int(content_length) > 5000000 #5mb but we can change the number if thats too big
+    else:
+        return False
 
 def follow_rules_of(url, file) -> bool:
     disallow = set()
